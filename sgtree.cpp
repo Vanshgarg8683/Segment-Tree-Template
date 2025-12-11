21aa9c268de8b1916bf9749f23664e67f7940926
Vansh Garg <vg9233384@gmail.com>
2025-12-11 23:26:07 +0530

    Update sgtree.cpp

--- sgtree.cpp
+++ sgtree.cpp
@@ -5,7 +5,7 @@ class sgtree{
         seg.resize(4*n+1,0);
         lazy.resize(4*n+1,0);
     }
-
+    // Build sgt
     void buildsgt(int ind, int low, int high, vector<int>&nums){
         if(low==high){
             seg[ind]=nums[low];
@@ -17,7 +17,18 @@ class sgtree{
         buildsgt(2*ind+2, mid+1, high, nums);
         seg[ind]=seg[2*ind+1]+seg[2*ind+2];
     }
-
+    // point update
+    void update(int ind, int low, int high, int &i, int &val){
+        if(low==high){
+            seg[ind]=val;
+            return;
+        }
+        int mid=(low+high)/2;
+        if(i<=mid) update(2*ind+1, low, mid, i, val);
+        else update(2*ind+2, mid+1, high, i, val);
+        seg[ind]=seg[2*ind+1]+seg[2*ind+2];
+    }
+    // Range Update(Lazy Propagation)
     void update(int ind, int low, int high, int &l, int &r, int &val){
         seg[ind] = seg[ind] + (high-low+1)*lazy[ind];
         if(low!=high){
@@ -43,6 +54,7 @@ class sgtree{
         seg[ind]=seg[2*ind+1]+seg[2*ind+2];
     }
 
+    // Query sum
     int query(int ind, int low, int high, int &l, int &r, vector<int>&nums){
         seg[ind] = seg[ind] + (high-low+1)*lazy[ind];
         if(low!=high){
