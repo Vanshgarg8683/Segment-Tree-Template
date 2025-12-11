9f9b53b2eac5c8aa8647ec24f722f241334e200f
Vansh Garg <vg9233384@gmail.com>
2025-12-11 23:23:18 +0530

    Update sgtree.cpp

--- sgtree.cpp
+++ sgtree.cpp
@@ -1,8 +1,9 @@
 class sgtree{
-    vector<int>seg;
     public:
+    vector<int>seg,lazy;
     sgtree(int n){
-        seg.resize(4*n+1);
+        seg.resize(4*n+1,0);
+        lazy.resize(4*n+1,0);
     }
 
     void buildsgt(int ind, int low, int high, vector<int>&nums){
@@ -17,18 +18,38 @@ class sgtree{
         seg[ind]=seg[2*ind+1]+seg[2*ind+2];
     }
 
-    void update(int ind, int low, int high, int &i, int &val){
-        if(low==high){
-            seg[ind]=val;
+    void update(int ind, int low, int high, int &l, int &r, int &val){
+        seg[ind] = seg[ind] + (high-low+1)*lazy[ind];
+        if(low!=high){
+            lazy[2*ind+1] += lazy[ind];
+            lazy[2*ind+2] += lazy[ind];
+        }
+        lazy[ind]=0;
+        
+        if(high<l || low>r) return;
+        
+        if(low>=l && high<=r){
+            seg[ind]+=(high-low+1)*val;
+            if(low!=high){
+                lazy[2*ind+1]+=val;
+                lazy[2*ind+2]+=val;
+            }
             return;
         }
+        
         int mid=(low+high)/2;
-        if(i<=mid) update(2*ind+1, low, mid, i, val);
-        else update(2*ind+2, mid+1, high, i, val);
+        update(2*ind+1, low, mid, l, r, val);
+        update(2*ind+2, mid+1, high, l, r, val);
         seg[ind]=seg[2*ind+1]+seg[2*ind+2];
     }
 
     int query(int ind, int low, int high, int &l, int &r, vector<int>&nums){
+        seg[ind] = seg[ind] + (high-low+1)*lazy[ind];
+        if(low!=high){
+            lazy[2*ind+1] += lazy[ind];
+            lazy[2*ind+2] += lazy[ind];
+        }
+        lazy[ind]=0;
         if(low>r || high<l) return 0;
         if(low>=l && high<=r) return seg[ind];
         int mid = (low + high)/2;
